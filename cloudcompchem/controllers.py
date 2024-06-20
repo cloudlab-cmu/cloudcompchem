3930c8f07031868da824aadb230fb76d8e252d0b
Yiwen Wang <yiwenwang9702@gmail.com>
2024-06-20 00:51:17 -0400

    geom opt endpoint

--- cloudcompchem/controllers.py
+++ cloudcompchem/controllers.py
@@ -12,7 +12,8 @@ from cloudcompchem.exceptions import (
     MoleculeSpinAndChargeViolationError,
     NotLoggedInException,
 )
-from cloudcompchem.models import EnergyRequest
+from cloudcompchem.models import EnergyRequest, DFTOptRequest
+from cloudcompchem.opt import run_dft_opt
 
 
 class DFTController:
@@ -112,6 +113,51 @@ class DFTController:
 
         self._logger.info("Received request to optimize a molecule!")
 
+        # Parse the request
+        try:
+            dft_input = self._parse_opt_request(global_request)
+        except MoleculeSpinAndChargeViolationError as err:
+            return (str(err), HTTPStatus.BAD_REQUEST)
+        except DFTRequestValidationException as err:
+            self._logger.error(f"Validation error: {err.message} Returning")
+            return (err.message, err.status_code)
+        except NotLoggedInException as err:
+            self._logger.error("Not logged in! Returning")
+            return (err.message, err.status_code)
+        except Exception as err:
+            self._logger.error(f"Unhandled exception: {err}")
+            return (
+                "Error encountered while unpacking request JSON, please inspect for errors and try again.",
+                HTTPStatus.INTERNAL_SERVER_ERROR,
+            )
+
+        # Download the needed information about each object - this will also make
+        # sure the request is properly formatted and contains information we have
+        # permission to see
+        self._logger.info(f"Received request to run DFT using: {dft_input}.")
+
+        # Trigger the simulation - we can do each one in parallel
+        # NOTE: this is not actually parallel in python since python has a GIL
+        # for this process, but we can handle additional IO requests
+        self._logger.info("Triggering dft simulation request")
+
+        try:
+            structure_dict = run_dft_opt(dft_input)
+        except (RuntimeError, KeyError) as err:
+            message = f"Runtime error encountered during DFT calculation due to misconfigured inputs: {err}"
+            self._logger.warning(message)
+            return message, HTTPStatus.BAD_REQUEST
+        except AssertionError:
+            message = "Found a runtime exception likely due to an invalid charge specification."
+            self._logger.warning(message)
+            return message, HTTPStatus.BAD_REQUEST
+        except Exception as err:
+            self._logger.error(f"Unhandled exception of type ({type(err)}): {err}.")
+            return f"Unhandled exception: {err}.", HTTPStatus.INTERNAL_SERVER_ERROR
+
+        return make_response(asdict(structure_dict), HTTPStatus.OK)
+
+
     def _parse_dft_request(self, request) -> EnergyRequest:
         """Parse the simulation request into the auth token, the protocols to
         simulation, and the model to use.
@@ -136,6 +182,34 @@ class DFTController:
             raise DFTRequestValidationException("No JSON body found, please include one to run a calculation.")
 
         dft_input = EnergyRequest.from_dict(req_info)
+
+        self._logger.info("Request constructed!")
+
+        return dft_input
+    
+    def _parse_opt_request(self, request) -> DFTOptRequest:
+        """Parse the geometry optimization request into the auth token, the protocols to
+        simulation, and the model to use.
+
+        Generally there should be no reason to update this function.
+        """
+        token = self._retrieve_auth_token_from_request(request)
+        self._logger.info("Got token from request! Attempting to validate token...")
+        self._constellation._auth_token = token
+        try:
+            _ = self._constellation.me()
+        except Exception:
+            raise NotLoggedInException("No authentication from login was provided!") from None
+        self._logger.info("Token validated!")
+
+        # unpack the request into a struct
+        req_info = request.json
+        self._logger.info("Attempting to unmarshal the request payload to internal struct...")
+        self._logger.debug(f"req info = {req_info}")
+        if req_info is None:
+            raise DFTRequestValidationException("No JSON body found, please include one to run a calculation.")
+
+        dft_input = DFTOptRequest.from_dict(req_info)
 
         self._logger.info("Request constructed!")
 
